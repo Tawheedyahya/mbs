025fe3ffe9c4f955dc99ab78093c1ddac42ad583
Tawheedyahya <tawheedyahya0@gmail.com>
2025-12-30 12:56:05 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+?>
